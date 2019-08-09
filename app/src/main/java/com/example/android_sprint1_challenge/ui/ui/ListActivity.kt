f95fed5b005fb4e40ca6e627ae857bf45d3d6628
corgipower <greensborodogtraining@gmail.com>
2019-08-09 12:33:37 -0400

    remove button from list xml, add text view

--- app/src/main/java/com/example/android_sprint1_challenge/ui/ui/ListActivity.kt
+++ app/src/main/java/com/example/android_sprint1_challenge/ui/ui/ListActivity.kt
@@ -1,13 +1,26 @@
 package com.example.android_sprint1_challenge.ui.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.android_sprint1_challenge.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class ListActivity : AppCompatActivity() {
+
+    //companion object for intent
+    companion object {
+        const val REQUEST_MOVIE_WATCHED = 10
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //button on list will open edit activity
+        movie_list_item.setOnClickListener {
+            var editMovieIntent = Intent(this, EditActivity::class.java)
+            startActivityForResult(editMovieIntent, REQUEST_MOVIE_WATCHED)
+        }
     }
 }
